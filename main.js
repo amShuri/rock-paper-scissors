733bdc6000e3dfb40178ffc7c3c316b99d7e5bd0
amShuri <juli.meep93@gmail.com>
2022-11-23 03:29:58 -0300

    Create function that tests the players choices

--- main.js
+++ main.js
@@ -3,3 +3,16 @@ function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     return rockPaperScissors[randomNumber];
 }
+
+
+//this part of the code seems a bit messy to me and I plan
+//to come back to it once we revisit the code on TOP's lesson
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'rock' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'paper') {
+        return `That's a tie. ${playerSelection} can't beat ${computerSelection}!`
+    } else if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'rock') {
+        return `You lose! ${computerSelection} beats ${playerSelection}!`;
+    } else if (playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper' || playerSelection === 'rock' && computerSelection === 'scissors'){
+        return `You win! ${playerSelection} beats ${computerSelection}!`;
+    }
+}
