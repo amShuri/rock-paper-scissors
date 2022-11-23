d44ebae7ce278bc984209a0f2953ddb6598508c1
amShuri <juli.meep93@gmail.com>
2022-11-23 03:31:21 -0300

    Create function that plays rock paper scissors

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ function getComputerChoice() {
     return rockPaperScissors[randomNumber];
 }
 
-
 //this part of the code seems a bit messy to me and I plan
 //to come back to it once we revisit the code on TOP's lesson
 function playRound(playerSelection, computerSelection) {
@@ -16,3 +15,26 @@ function playRound(playerSelection, computerSelection) {
         return `You win! ${playerSelection} beats ${computerSelection}!`;
     }
 }
+
+//There's a TypeError when the prompt's value is null
+//because the string methods can't be applied to it (I think),
+//I don't know how to fix this yet so I'll come back to it later too
+function game() {
+    const pattern = /\s/g;
+
+    for(let i = 0; i < 5; i++) {
+        const computerChoice = getComputerChoice();
+        let playerChoice = prompt('Enter your choice', 'paper').toLowerCase().replace(pattern, '');
+        //when value is null the prompt throws a TypeError because of the string method
+        //will come back to this later
+        if(playerChoice !== 'rock' && playerChoice !== 'scissors' && playerChoice !== 'paper') {
+            i--;
+            console.warn('Wrong input. Try again.');
+        } else{
+            console.log(`Round number ${i + 1}: ` + playRound(playerChoice, computerChoice));
+        }
+        
+    }
+}
+
+game()
