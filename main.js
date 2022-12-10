2ea61b3361dbb7cfe1e38f586af5682eb2401085
amShuri <juli.meep93@gmail.com>
2022-12-10 18:18:46 -0300

    Update playRound function to log players' scores

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+let playerScore = 0;
+let computerScore = 0;
+let maxScore = 5;
+const buttons = document.querySelectorAll('.gameButton');
+for(const button of buttons) {
+    button.addEventListener('click', playRound);
+}
+
 function getComputerChoice() {
     const rockPaperScissors = ['rock', 'paper', 'scissors'];
     const randomNumber = Math.floor(Math.random() * 3);
@@ -14,18 +22,32 @@ function getPlayerChoice() {
 function playRound() {
     const playerChoice = getPlayerChoice();
     const computerChoice = getComputerChoice();
-    const matchResults = document.querySelector('#match-result');
+    const matchResult = document.querySelector('#match-result');
 
     if (playerChoice === computerChoice) {
-        matchResults.textContent = `That's a tie! Both player chose ${playerChoice  }!`;
+        matchResult.textContent = `That's a tie! Both player chose ${playerChoice  }!`;
     } else if (playerChoice === 'rock' && computerChoice === 'scissors' || playerChoice === 'paper' && computerChoice === 'rock' || playerChoice === 'scissors' && computerChoice === 'paper') {
-        matchResults.textContent = `You win! Your ${playerChoice} beats the player's ${computerChoice}!`;
+        playerScore++;
+        matchResult.textContent = `You win! Your ${playerChoice} beats the player's ${computerChoice}!`;
+        if(playerScore === maxScore) {
+            matchResult.textContent = `GAME IS OVER! You won. Your score is: ${playerScore}!`;
+        }
     } else {
-        matchResults.textContent = `Opponent wins! Their ${computerChoice} beats your ${playerChoice}!`;
+        computerScore++;
+        matchResult.textContent = `Opponent wins! Their ${computerChoice} beats your ${playerChoice}!`;
+        if(computerScore === maxScore) {
+            matchResult.textContent = `GAME IS OVER! Your opponent won. Their score is: ${computerScore}!`;
+        }
     }
-}
 
-const buttons = document.querySelectorAll('.gameButton');
-for(const button of buttons) {
-    button.addEventListener('click', playRound);
+    if(playerScore === maxScore || computerScore === maxScore) {
+        for(const button of buttons) {
+            button.disabled = true;
+        }
+    }
+
+    const playerInfo = document.querySelector('#player-score');
+    const computerInfo = document.querySelector('#computer-score');
+    playerInfo.textContent = `Player's Score is ${playerScore}!`;
+    computerInfo.textContent = `Computer's Score is ${computerScore}!`;
 }
